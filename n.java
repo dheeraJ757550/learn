883384e9ff86a11d9d1c4fab342a787141464565
Dheeraj Kumar <chhabradheeru75@gmail.com>
2024-04-13 01:24:14 +0530

    nitial

---
+++ n.java
@@ -0,0 +1,15 @@
+package learn;
+public class n {
+    static void n(int n){
+        System.out.println("only n");
+    }
+    static void n(int n , int m){
+        System.out.println("m and n given");
+    }
+    public static void main(String[] args) {
+        n(0);
+        n(4,5);
+        
+    }
+    
+}
