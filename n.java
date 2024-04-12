4db88f29fdf2b4219c2301b933f5be684c81fc78
dheeraJ757550 <135153358+dheeraJ757550@users.noreply.github.com>
2024-04-13 01:41:26 +0530

    Update n.java

--- n.java
+++ n.java
@@ -1,14 +1,1 @@
-package learn;
-public class n {
-    static void n(int n){
-        System.out.println("only n");
-    }
-    static void n(int n , int m){
-        System.out.println("m and n  dheerajjjjjjjjjjj issssssssssss given");
-    }
-    public static void main(String[] args) {
-        n(0);
-        n(4,5);
-        
-    }
-}
+
